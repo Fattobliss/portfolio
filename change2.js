7849ca51dca54b5a478e811c28f1523c570023a2
akshay <akshaysunder2002@gmail.com>
2024-08-26 16:46:19 +0530

    added

---
+++ change2.js
@@ -0,0 +1,6 @@
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    // Generate a random color
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // Change the background color
+    document.body.style.backgroundColor = randomColor;
+});
